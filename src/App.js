59cbc8448ae75f2b5dad7a4b562f572959f9ae94
RajasriJanaki <rajasrijr55@gmail.com>
2023-07-05 16:45:53 -0700

    feat: add assets and header with styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import './App.scss';
+import Logo from "./assets/Logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      "Homepage"
+    <div className="main-grid">
+      <header>
+        <img id="logo" src={Logo} alt="Little Lemon logo"/>
+        <nav>
+          <ul class="nav-links">
+            <li><a href="/home">Homepage</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/blog">Blog</a></li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
